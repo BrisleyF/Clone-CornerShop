37ea880a237187c9ec42d5a1b502216647e6c0d2
BrisleyF <fajardobrisley@gmail.com>
2022-09-26 18:34:32 -0400

    Vista de  productos

--- server.js
+++ server.js
@@ -46,9 +46,11 @@ app.get('/tiendas/:name', (req, res) => {
     const name = req.params.name;
 
     let store = {
+        id: 'jumbo',
         name: 'Jumbo',
         products: [
             {
+                id: 'bananas',
                 name: 'Bananas',
                 image: 'https://s.cornershopapp.com/product-images/1111602.jpe?versionId=CqCXpLFuPgpp1s9pXKwwrWIXCaNXr5yS',
                 price: '1$',
@@ -56,15 +58,17 @@ app.get('/tiendas/:name', (req, res) => {
                 unit: 'Unidad: 500gr aprox',
             },
             {
+                id: 'tomate',
                 name: 'Jumbo - Tomate chonto',
-                image: 'https://s.cornershopapp.com/product-images/1111602.jpe?versionId=CqCXpLFuPgpp1s9pXKwwrWIXCaNXr5yS',
+                image: 'https://s.cornershopapp.com/product-images/1111674.jpg?versionId=3EZvnU4z5YvQlwYEi.pL2HlE8zH4lygy',
                 price: '1.5$',
                 unitDescription: 'Precio por kg',
                 unit: 'Unidad: 300gr aprox',
             },
             {
+                id: 'cebolla',
                 name: 'Cebolla cabezona',
-                image: 'https://s.cornershopapp.com/product-images/1111602.jpe?versionId=CqCXpLFuPgpp1s9pXKwwrWIXCaNXr5yS',
+                image: 'https://s.cornershopapp.com/product-images/1111787.jpg?versionId=N.zb97lxOQZJ5OtMM9ECVSpmFBcBtHoG',
                 price: '1.8$',
                 unitDescription: 'Precio por kg',
                 unit: 'Unidad: 240gr aprox',
@@ -74,21 +78,25 @@ app.get('/tiendas/:name', (req, res) => {
     
     if (name === 'spid') {
         store = {
+            id: 'spid',
             name: 'Spid',
             products: [
                 {
+                    id: 'vainilla',
                     name: 'Levapan - Esencia sabor a vainilla caramelo',
                     price: '3$',
                     image: 'https://s.cornershopapp.com/product-images/6198689.jpg?versionId=VZ9lVBCHLaXYJgA3GP00_QA3w0RZu7vz',
                     unit: 'Botella 60 ml',
                 },
                 {
+                    id: 'nescafe',
                     name: 'Nescafe - Cafe descafeinado',
                     image: 'https://s.cornershopapp.com/product-images/1061964.jpg?versionId=Tm3NHkQQQEPye8.wYGei1DR9ObBZQEGB',
                     price: '6$',
                     unit: 'Frasco de 100 g',
                 },
                 {
+                    id: 'maruchan',
                     name: 'Maruchan - Sopa de res',
                     image: 'https://s.cornershopapp.com/product-images/1060734.jpg?versionId=.rjLlOi_0aIjhRjIDZ9GMcTU.0I.JKWT',
                     price: '2$',
@@ -100,9 +108,11 @@ app.get('/tiendas/:name', (req, res) => {
 
     if (name === 'alkosto') {
         store = {
+            id: 'alkosto',
             name: 'SUPER Alkosto',
             products: [
                 {
+                    id: 'producto-1',
                     name: 'Alkosto producto 1',
                     price: '3$',
                     image: 'https://s.cornershopapp.com/product-images/6198689.jpg?versionId=VZ9lVBCHLaXYJgA3GP00_QA3w0RZu7vz',
@@ -114,6 +124,52 @@ app.get('/tiendas/:name', (req, res) => {
 
 	res.render('tienda', store);
 });
+
+app.get('/tiendas/:store/:product', (req, res) => {
+    const storeId = req.params.store;
+    const productId = req.params.product;
+
+    let producto = {
+        name: 'Bananas',
+        image: 'https://s.cornershopapp.com/product-images/1111602.jpe?versionId=CqCXpLFuPgpp1s9pXKwwrWIXCaNXr5yS',
+        price: '1$',
+        unitDescription: 'Precio por kg',
+        unit: 'Unidad: 500gr aprox',
+    }
+
+    if (productId === 'vainilla') {
+        producto = {
+            name: 'Vainilla',
+            image: 'https://s.cornershopapp.com/product-images/6198689.jpg?versionId=VZ9lVBCHLaXYJgA3GP00_QA3w0RZu7vz',
+            price: '3$',
+            unitDescription: 'Precio por ml',
+            unit: 'Botella 60 ml',
+        }
+    }
+    
+    if (productId === 'nescafe') {
+        producto = {
+            name: 'Nescafe',
+            image: 'https://s.cornershopapp.com/product-images/1061964.jpg?versionId=Tm3NHkQQQEPye8.wYGei1DR9ObBZQEGB',
+            price: '6$',
+            unitDescription: 'Precio por kg',
+            unit: 'Frasco de 100 g',
+        }
+    }
+
+    if (productId === 'maruchan') {
+        producto = {
+            name: 'Marucgan - sopa de res',
+            image: 'https://s.cornershopapp.com/product-images/1060734.jpg?versionId=.rjLlOi_0aIjhRjIDZ9GMcTU.0I.JKWT',
+            price: '2$',
+            unitDescription: 'Precio por kg',
+            unit: 'Carton de 64 g',
+        }
+    }
+
+	res.render('producto', { producto, storeId });
+});
+
 
 const PUERTO = process.env.PORT || 3000;
 
